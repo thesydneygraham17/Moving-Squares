86d917ed256e6cbf07188e1c4a0fa4485d825e35
SYDNEY GRAHAM <SLG22F@FSU.EDU>
2023-09-19 21:35:22 -0400

    First Base Animation

---
+++ sketch copy.js
@@ -0,0 +1,7 @@
+function draw(){
+  background(bgcolor);
+
+  if(keyIsPressed == true){
+    rect(mouseX, 100, 50, 50);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+let x = 200;
+let speed = 50;
+let diameter = 40;
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(125);
+  background("lavender");
+  ellipse( x, height/2, diameter);
+  if(keyIsPressed == true);
+  if(x >= width || x <= 0) {
+    speed= speed * -1;
+    
+  }
+  x+= speed;
 }
